6579cac57d9ba50682802bbef0a07e37323bf630
Ahmed Ali <a.husseinzaki@gmail.com>
2016-03-23 23:39:23 -0700

    feat: Change enemies count to 3

--- js/app.js
+++ js/app.js
@@ -169,7 +169,7 @@ function transformEntityLocToPic(location) {
 }
 
 function generateEnemies() {
-    for(var i=0; i < 5; i++) {
+    for(var i=0; i < 3; i++) {
         allEnemies.push(new Enemy());
     }
 }
